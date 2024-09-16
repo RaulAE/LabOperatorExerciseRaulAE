94538802479d0789e232f4611ee485780595a9b0
Raul Aquino Escobar <raquinoescobar@students.mchenry.edu>
2024-09-15 19:26:46 -0500

    Crititcal Thinking Exercise where I practied using operators

---
+++ OperatorExercise.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// Section 1: Variables
+
+let dollar = 1.00
+let dime = 0.10
+let quarter = 0.25
+
+let discount = 0.5
+
+let per_num = 5
+let per_num2 = 2
+
+// Section 2 Operations
+
+var subtraction = dollar - dime
+var addition = dime + quarter
+var multiplication = dollar * discount
+var division = dollar / discount
+var percent = per_num % per_num2
+
+// Section 3 results
+
+print(subtraction)
+print(addition)
+print(multiplication)
+print(division)
+print(percent)
